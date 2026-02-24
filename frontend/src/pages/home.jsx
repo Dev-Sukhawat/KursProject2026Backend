39746bcb77247b16a773f3e4664a9ba84d59e04d
Sukhawat <sukhawat.charoenchit@gmail.com>
2026-02-24 12:35:00 +0100

    feat: add Intro, Why, Starter, and Footer components to Home page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,9 +1,17 @@
 import Header from "../components/homepage/header";
+import Intro from "../components/homepage/intro";
+import Why from "../components/homepage/why";
+import Starter from "../components/homepage/starter";
+import Footer from "../components/footer/footer";
 
 function Home() {
   return (
     <>
       <Header />
+      <Intro />
+      <Why />
+      <Starter />
+      <Footer />
     </>
   );
 }
