d8c68fa0370caa7c424984fe7668f9b133e833ef
Sukhawat <sukhawat.charoenchit@gmail.com>
2026-02-27 16:30:34 +0100

    Initialize backend with Express and CORS setup

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+// https://www.youtube.com/watch?v=yc_D8u7ETuw
+import express from "express";
+import cors from "cors";
+const app = express();
+
+app.listen(8080, () => {
+    console.log("Server is running on port 8080");
+})
