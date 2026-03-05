beac47b08ab20a1d9acd79c8dc82c3522ad316a9
Dev-Sukhawat <Sukhawat.charoenchit@gmail.com>
2026-03-05 14:09:54 +0100

    feat: add Booking and BookingManagement components with layout structure

---
+++ frontend/src/pages/BookingManagement.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/navbar/navbar";
+import Footer from "../components/footer/footer";
+import Booking from "../components/admin/Booking";
+
+export default function BookingManagement() {
+  return (
+    <>
+      <Navbar>
+        <Booking/>
+      </Navbar>
+      <Footer />
+    </>
+  );
+}
