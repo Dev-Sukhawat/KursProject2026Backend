ef22b44196ebd0c537d622e879d28144cc49bf1e
Sukhawat <sukhawat.charoenchit@gmail.com>
2026-03-12 21:26:16 +0100

    feat: update key

--- backend/config/supabaseClient.js
+++ backend/config/supabaseClient.js
@@ -11,12 +11,12 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, "../.env") });
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 // Kontrollera att variablerna faktiskt finns
-if (!supabaseUrl || !supabaseKey) {
+if (!supabaseUrl || !supabaseServiceKey) {
     console.error("❌ Error: SUPABASE_URL or SUPABASE_ANON_KEY is missing in .env");
 }
 
 // Skapa och exportera klienten
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseServiceKey);
