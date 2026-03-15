6e0a0afb289a4388a373709943bb5d4850861aa5
Sukhawat <sukhawat.charoenchit@gmail.com>
2026-03-15 21:06:40 +0100

    rm: on wan't usedate

--- frontend/src/components/user/UserProfile.jsx
+++ frontend/src/components/user/UserProfile.jsx
@@ -3,7 +3,7 @@ import Header from "./UserProfile/header";
 import UseInfo from "./UserProfile/userinfo";
 
 export default function UserProfile() {
-  const { rooms, bookings } = useData();
+  const { bookings } = useData();
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <Header />
